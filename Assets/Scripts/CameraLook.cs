4a960f81446cee0888d100f0a54ff20dfe1fae79
Sinclair <sinfram248@gmail.com>
2019-05-01 12:04:35 +1000

    Added Camera Movement

--- Assets/Scripts/CameraLook.cs
+++ Assets/Scripts/CameraLook.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraLook : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class CameraLook : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public bool cursorHidden = true;
+	public float minYAngle = -80f, maxYAngle = 80f;
+	public Vector2 speed = new Vector2(120f, 120f);
+
+	Vector2 euler;
+
+	void Start() {
+		
+		if (cursorHidden) {
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+
+		euler = transform.eulerAngles;
+	}
+
+	void Update() {
+
+		euler.y += Input.GetAxis("Mouse X") * speed.x * Time.deltaTime;
+		euler.x -= Input.GetAxis("Mouse Y") * speed.y * Time.deltaTime;
+
+		euler.x = Mathf.Clamp(euler.x, minYAngle, maxYAngle);
+
+		transform.parent.localEulerAngles = Vector3.up * euler.y;
+		transform.localEulerAngles = Vector3.right * euler.x;
+	}
 }
---
+++ Assets/Scripts/LookAtDirection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtDirection : MonoBehaviour {
+
+	public Transform target;
+
+	void LateUpdate() {
+		transform.rotation = Quaternion.LookRotation(target.forward);
+	}
+}
