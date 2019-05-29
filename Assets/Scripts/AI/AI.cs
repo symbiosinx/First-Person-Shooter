98b0160196c5f76b4be528588435dd1281197489
Sinclair <sinfram248@gmail.com>
2019-05-29 12:08:14 +1000

    Added Steering Behaviours

---
+++ Assets/Scripts/AI/AI.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour {
+
+	Vector3 velocity;
+	protected float maxVelocity = 5f;
+	protected float maxDistance = 5f;
+	protected NavMeshAgent agent;
+	protected SteeringBehaviour[] behaviours;
+
+	public Vector3 Velocity {
+		protected set { velocity = value; }
+		get { return velocity; }
+	}
+
+	void Update() {
+		CalculateForce();
+	}
+
+	void Awake() {
+		agent = GetComponent<NavMeshAgent>();
+		behaviours = GetComponents<SteeringBehaviour>();
+	}
+
+	public virtual Vector3 CalculateForce() {
+		Vector3 force = Vector3.zero;
+
+		foreach (var behaviour in behaviours) {
+			force += behaviour.GetForce() * behaviour.weighting;
+			if (force.magnitude > maxVelocity) {
+				force = force.normalized * maxVelocity;
+				break;
+			}
+		}
+
+		velocity += force * Time.deltaTime;
+
+		if (velocity.magnitude > maxVelocity) {
+			velocity = velocity.normalized * maxVelocity;
+		}
+
+		if (velocity.magnitude > 0) {
+			Vector3 pos = transform.position + velocity * Time.deltaTime;
+			NavMeshHit hit;
+			if (NavMesh.SamplePosition(pos, out hit, maxDistance, -1)) {
+				agent.SetDestination(hit.position);
+			}
+		}
+		return force;
+	}
+
+}
---
+++ Assets/Scripts/AI/SteeringBehaviours/Seek.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seek : SteeringBehaviour {
+
+	public Transform target;
+	public float stoppingDistance;
+
+	public override Vector3 GetForce() {
+		Vector3 force = Vector3.zero;
+
+		if (target != null) {
+			Vector3 desiredForce = target.position;
+			if (desiredForce.magnitude > stoppingDistance) {
+				desiredForce = desiredForce.normalized * weighting;
+				force = desiredForce - owner.Velocity;
+			}
+		}
+
+		return force;
+	}
+
+}
